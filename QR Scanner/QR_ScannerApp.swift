db2a6cb0b9ce1d73e0b116785db4f79f76c67831
Ctrl-Alt-Tea <dylanrose.sa@gmail.com>
2024-04-30 17:14:16 +0200

    First commit

---
+++ QR Scanner/QR_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QR_ScannerApp.swift
+//  QR Scanner
+//
+//  Created by Dylan Rose on 2024/04/10.
+//
+
+import SwiftUI
+
+@main
+struct QR_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
